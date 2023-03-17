2cf4a0d3ebad82420a931e25a49e0455c512bc33
Hydley F <hydley.jailsonk@gmail.com>
2023-03-17 00:06:03 -0300

    IF's are now a thing.

--- HTPL.js
+++ HTPL.js
@@ -1,5 +1,8 @@
 // ---------------------------------------------------------------------------------
 
+globalVariables = {}
+globalFunctions = {}
+
 document.addEventListener('DOMContentLoaded', () => {
 	const blocks = document.querySelectorAll('code');
 	blocks.forEach(block => execute(block))
@@ -7,27 +10,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // ---------------------------------------------------------------------------------
 
-directCompileInstructions = ['variable', 'if', 'print']
-indirectCompileInstructions = ['function']
-variables = {}
-functions = {
-	'print': (content) => {console.log(content);}
-}
-
 function execute(block)
 {
 	Array.from(block.children).forEach(element => {
 		instruction = element.tagName.toLowerCase();
 
-		if(!directCompileInstructions.includes(instruction))
-		{
-			return;
-		}
-
 		compile(instruction, element);
 	})
 }
 
+// Direct compile instructions
 function compile(instruction, element)
 {
 	switch(instruction)
@@ -46,6 +38,21 @@ function compile(instruction, element)
 			implementPrint(element);
 
 			break;
+
+		case 'function':
+			// let arguments = {};
+			// for (attribute in element.attributes)
+			// {
+			// 	const att = element.attributes[attribute];
+				
+			// 	if(!att.value || att.nodeName == 'name') continue;
+
+			// 	arguments[att.nodeName] = att.ArraynodeValue;
+			// }
+
+			implementFunction(element)//, arguments);
+
+			break;
 	}
 }
 
@@ -53,19 +60,20 @@ function compile(instruction, element)
 
 function implementVariable(element)
 {
+	// Variable load
 	if(!element.childNodes[0] & element.hasAttributes())
 	{
-		if(!element.attributes.name.nodeValue in variables)
+		if(!element.attributes.name.nodeValue in globalVariables)
 		{
 			throw new Error(`Variable ${element.attributes.name.nodeValue} does not exists.`);
-			return;
 		}
 
-		return variables[element.attributes.name.nodeValue];
+		return globalVariables[element.attributes.name.nodeValue];
 	}
 
 	if(!element.hasAttributes() & !element.childNodes[0]) throw new Error('Cannot asign an empty value at ' + `${element.tagName}`);
 
+	// Variable definition
 	let value = undefined;
 
 	if('type' in element.attributes)
@@ -104,9 +112,9 @@ function implementVariable(element)
 		}
 	}
 
-	// console.log(variables);
+	// console.log(globalVariables);
 
-	if('name' in element.attributes) variables[element.attributes.name.nodeValue] = value;
+	if('name' in element.attributes) globalVariables[element.attributes.name.nodeValue] = value;
 	
 	return value;
 }
@@ -118,9 +126,9 @@ function implementIf(element)
 	let condition = element.attributes.condition.nodeValue.replace('=', '==');
 
 	condition.split(' ').forEach((word) => {
-		if(word in variables)
+		if(word in globalVariables)
 		{
-			condition = condition.replace(word, variables[word]);
+			condition = condition.replace(word, globalVariables[word]);
 		}
 	});
 
@@ -155,5 +163,54 @@ function implementPrint(element)
 {
 	if(!element.childNodes) throw new Error(`Cannot print empty at ${element.tagName}`);
 
-	console.log(element.childNodes[0].nodeValue);
+	result = '';
+
+	element.childNodes.forEach((element) =>
+	{
+		if(element.nodeName == '#text')
+		{
+			result += element.textContent;
+		}
+		else if(element.nodeName == 'VARIABLE')
+		{
+			result += implementVariable(element);
+		}
+	})
+
+	console.log(result);
+}
+
+function implementFunction(element, arguments)
+{
+	// Function call
+	if(!element.childNodes[0])
+	{
+
+		if(!element.attributes.name.nodeValue in globalFunctions)
+		{
+			throw new Error(`Function ${element.attributes.name.nodeValue} does not exists in this context.`);
+			return;
+		}
+
+		for(variable in arguments)
+		{
+			if(variable in globalVariables) throw new Error(`"${variable}" already exists in the global scope.`);
+
+			globalVariables[variable] = arguments[variable];
+		}
+
+		execute(globalFunctions[element.attributes.name.nodeValue]);
+
+		for(variable in arguments)
+		{
+			globalVariables.delete(variable);
+		}
+
+		return;
+	}
+
+	// Function definition
+	if(!element.hasAttributes() & !element.childNodes[0]) throw new Error('Cannot asign an empty function at ' + `${element.tagName}`);
+
+	globalFunctions[element.attributes.name.nodeValue] = element;
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # HyperText-Programming-Language
  Who said HTML wasn't a programming language?
 
-Inspired in [ryangjchandler's](https://github.com/ryangjchandler) [Cursed HTML idea](https://github.com/ryangjchandler/cursed-html).
+Heavily inspired by [ryangjchandler's](https://github.com/ryangjchandler) [Cursed HTML idea](https://github.com/ryangjchandler/cursed-html).
