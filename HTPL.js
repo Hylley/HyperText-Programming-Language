e408e2de044b929bd2010a33e8534cab2343f3b8
Hydley F <hydley.jailsonk@gmail.com>
2023-03-16 22:23:23 -0300

    Initial commit

--- HTPL.js
+++ HTPL.js
@@ -1,8 +1,125 @@
-const boot = () => {
-	const blocks = document.querySelectorAll('code');
-	blocks.forEach(block => execute(block))
-}
+// ---------------------------------------------------------------------------------
 
 document.addEventListener('DOMContentLoaded', () => {
-    boot();
+	const blocks = document.querySelectorAll('code');
+	blocks.forEach(block => execute(block))
 })
+
+// ---------------------------------------------------------------------------------
+
+directCompileInstructions = ['variable', 'if']
+indirectCompileInstructions = ['function']
+variables = {}
+functions = {
+	'print': (content) => {console.log(content);}
+}
+
+function execute(block)
+{
+	Array.from(block.children).forEach(element => {
+		instruction = element.tagName.toLowerCase();
+
+		if(!directCompileInstructions.includes(instruction))
+		{
+			return;
+		}
+
+		compile(instruction, element);
+	})
+}
+
+function compile(instruction, element)
+{
+	switch(instruction)
+	{
+		case 'variable':
+			implementVariable(element);
+
+			break;
+		
+		case 'if':
+			implementIf(element);
+
+			break;
+	}
+}
+
+// ---------------------------------------------------------------------------------
+
+function implementVariable(element)
+{
+	if(!element.childNodes[0] & element.hasAttributes())
+	{
+		if(!element.attributes.name.nodeValue in variables)
+		{
+			throw new Error(`Variable ${element.attributes.name.nodeValue} does not exists.`);
+			return;
+		}
+
+		return variables[element.attributes.name.nodeValue];
+	}
+
+	if(!element.hasAttributes() & !element.childNodes[0]) throw new Error('Cannot asign an empty value at ' + `${element.tagName}`);
+
+	let value = undefined;
+
+	if('type' in element.attributes)
+	{
+		switch(element.attributes.type.nodeValue)
+		{
+			case 'string':
+				value = element.childNodes[0].nodeValue;
+				break;
+
+			case 'number':
+				value = +element.childNodes[0].nodeValue;
+				break;
+
+			case 'int':
+				value = parseInt(element.childNodes[0].nodeValue);
+				break;
+			
+			case 'float':
+				value = parseFloat(element.childNodes[0].nodeValue);
+				break;
+			
+			default:
+				throw new Error('Invalid variable type in ' + `${element.tagName}`);
+		}
+	}
+	else
+	{
+		if(isNaN(element.childNodes[0].nodeValue))
+		{
+			value = element.childNodes[0].nodeValue;
+		}
+		else
+		{
+			value = +element.childNodes[0].nodeValue;
+		}
+	}
+
+	console.log(variables);
+
+	if('name' in element.attributes) variables[element.attributes.name.nodeValue] = value;
+	
+	return value;
+}
+
+function implementIf(element)
+{
+	console.log(element);
+	if(!element.hasAttributes() || !'condition' in element.attributes) throw new Error(`Empty condition at ${element.tagName}`);
+
+	let condition = element.attributes.condition.nodeValue;
+	// const block = 
+
+	condition.split(' ').forEach((word) => {
+		if(word in variables)
+		{
+			condition = condition.replace(word, variables[word]);
+		}
+	});
+
+	
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # HyperText-Programming-Language
  Who said HTML wasn't a programming language?
+
+Inspired in [ryangjchandler's](https://github.com/ryangjchandler) [Cursed HTML idea](https://github.com/ryangjchandler/cursed-html).
